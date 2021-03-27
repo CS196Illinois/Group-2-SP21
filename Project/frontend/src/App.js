2675621aeca117cbfe1b9eede879f1a9f881aad2
ritika17135 <ritikap2@illinois.edu>
2021-03-27 15:35:45 -0700

    changes to rank-card

--- Project/frontend/src/App.js
+++ Project/frontend/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import RankCard from './components/RankCard'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let sampledata = [
+    {rank: 12, name: "Bitcoin", imageurl: 'https://images.theconversation.com/files/194266/original/file-20171113-27585-1gdvg8x.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=496&fit=clip'},
+    {rank: 12, name: "Bitcoin", imageurl: 'https://images.theconversation.com/files/194266/original/file-20171113-27585-1gdvg8x.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=496&fit=clip'}
+  ]
+
+  const renderCard = (card, index) => {
+    return (
+      <div className='rank-card'>
+          <RankCard rank={card.rank} name = {card.name} index = {index} imageurl = {card.imageurl}>
+          </RankCard>
+      </div>
+    );
+  };
+
+  return <div className="rank-card">{sampledata.map(renderCard)}</div>;
 }
 
 export default App;
