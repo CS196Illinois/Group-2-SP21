faf7a7eb2080b1350ba6def2a656f002c1e5e6c1
ritika17135 <ritikap2@illinois.edu>
2021-04-04 23:14:17 -0700

    setup frontend api calls

--- Project/frontend/src/App.js
+++ Project/frontend/src/App.js
@@ -1,23 +1,12 @@
 import './App.css';
-import RankCard from './components/RankCard'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react';
+import RankPage from './components/RankPage'
 
 function App() {
-  let sampledata = [
-    {rank: 12, name: "Bitcoin", imageurl: 'https://images.theconversation.com/files/194266/original/file-20171113-27585-1gdvg8x.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=496&fit=clip'},
-    {rank: 12, name: "Bitcoin", imageurl: 'https://images.theconversation.com/files/194266/original/file-20171113-27585-1gdvg8x.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=496&fit=clip'}
-  ]
-
-  const renderCard = (card, index) => {
-    return (
-      <div className='rank-card'>
-          <RankCard rank={card.rank} name = {card.name} index = {index} imageurl = {card.imageurl}>
-          </RankCard>
-      </div>
-    );
-  };
-
-  return <div className="rank-card">{sampledata.map(renderCard)}</div>;
+  return (
+    <RankPage />
+  )
 }
 
 export default App;
